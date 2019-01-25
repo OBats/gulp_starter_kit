70b42ffd11e5f8eed7548faf535835defa1fa516
OBats <orest.bats@gmail.com>
2019-01-25 09:43:19 +0200

    Remove unnecessary src path's

--- gulp/config.js
+++ gulp/config.js
@@ -1,58 +1,55 @@
 const minimist = require("minimist")(process.argv.slice(2));
-const color    = require("ansi-colors");
-const log      = require("fancy-log");
+const color = require("ansi-colors");
+const log = require("fancy-log");
 
 const production = minimist.production || minimist.prod || false;
-const destPath   = "build";
+const destPath = "build";
 
 let config = {
-    env        : "development",
-    production : production,
+  env: "development",
+  production: production,
 
-    src: {
-        root         : "src",
-        // templates    : "src/templates",
-        // templatesData: "src/templates/data",
-        // pagelist     : "src/index.yaml",
-        sass         : "src/sass",
-        css          : "src/css",
-        js           : "src/js",
-        uglyJs       : "src/js/unminified",
-        img          : "src/img",
-        svg          : "src/img/svg",
-        icons        : "src/img/icons",
-        // path to png sources for sprite:png task
-        iconsPng     : "src/img/icons",
-        // path to svg sources for sprite:svg task
-        iconsSvg     : "src/img/icons",
-        fonts        : "src/fonts",
-        lib          : "src/lib"
-    },
-    dest: {
-        root  : destPath,
-        html  : destPath,
-        css   : destPath + "/css",
-        js    : destPath + "/js",
-        img   : destPath + "/img",
-        fonts : destPath + "/fonts",
-        lib   : destPath + "/lib"
-    },
+  src: {
+    root: "src",
+    sass: "src/sass",
+    css: "src/css",
+    js: "src/js",
+    uglyJs: "src/js/unminified",
+    img: "src/img",
+    svg: "src/img/svg",
+    icons: "src/img/icons",
+    // path to png sources for sprite:png task
+    iconsPng: "src/img/icons",
+    // path to svg sources for sprite:svg task
+    iconsSvg: "src/img/icons",
+    fonts: "src/fonts",
+    lib: "src/lib"
+  },
+  dest: {
+    root: destPath,
+    html: destPath,
+    css: destPath + "/css",
+    js: destPath + "/js",
+    img: destPath + "/img",
+    fonts: destPath + "/fonts",
+    lib: destPath + "/lib"
+  },
 
-    setEnv: function(env) {
-        if (typeof env !== "string") return;
-        this.env = env;
-        this.production = env === "production";
-        process.env.NODE_ENV = env;
-    },
+  setEnv: function (env) {
+    if (typeof env !== "string") return;
+    this.env = env;
+    this.production = env === "production";
+    process.env.NODE_ENV = env;
+  },
 
-    logEnv: function() {
-        log(
-            "Environment:",
-            config.production ? color.black.bgYellow(" " + process.env.NODE_ENV + " ") : color.white.bgRed(" " + process.env.NODE_ENV + " ")
-        );
-    },
+  logEnv: function () {
+    log(
+      "Environment:",
+      config.production ? color.black.bgYellow(" " + process.env.NODE_ENV + " ") : color.white.bgRed(" " + process.env.NODE_ENV + " ")
+    );
+  },
 
-    errorHandler: require("./util/handle-errors")
+  errorHandler: require("./util/handle-errors")
 };
 
 config.setEnv(production ? "production" : "development");
